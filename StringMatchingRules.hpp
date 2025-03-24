ba428064638e629ae86ed175ab234e47d08f5883
LarissaCutti <cuttilarissa@gmail.com>
2025-03-24 10:43:40 -0300

    "Atulização do nome da variável de comprimento da"

--- StringMatchingRules.hpp
+++ StringMatchingRules.hpp
@@ -1,10 +1,10 @@
 //Retorna o número de diferentes caracteres entre duas strings 
-size_t HammingDistance(const string x, const string y, const size_t lenght);//funcionando para strings digitadas não pelo usuário
+size_t HammingDistance(const string x, const string y, const size_t length);//funcionando para strings digitadas não pelo usuário
 
-size_t BinaryDistanceA(const string x, const string y, const size_t lenght);
-size_t BinaryDistanceB(const string x, const string y, const size_t lenght);
-size_t BinaryDistanceC(const string x, const string y, const size_t lenght);
-size_t BinaryDistanceD(const string x, const string y, const size_t lenght);
+size_t BinaryDistanceA(const string x, const string y, const size_t length);
+size_t BinaryDistanceB(const string x, const string y, const size_t length);
+size_t BinaryDistanceC(const string x, const string y, const size_t length);
+size_t BinaryDistanceD(const string x, const string y, const size_t length);
 
 double RusselRao(const size_t a, const size_t b, const size_t c, const size_t d);
 double JacardNeedham(const size_t a, const size_t b, const size_t c);
@@ -16,7 +16,7 @@ double Yule(const size_t a, const size_t b, const size_t c, const size_t d);
 size_t editDistance(); //recursivo 
 //olhar implementação https://www.geeksforgeeks.org/edit-distance-dp-5/
 
-void valueDifferenceMetric(const string x, const string y, const size_t lenght);
-void vdm(const char xi, const char yi, const string C, const size_t lenght);
-void weight(const char xi, const string C, const size_t lenght);
-void P(const char ai, const string C, const size_t lenght);
+void valueDifferenceMetric(const string x, const string y, const size_t length);
+void vdm(const char xi, const char yi, const string C, const size_t length);
+void weight(const char xi, const string C, const size_t length);
+void P(const char ai, const string C, const size_t length);
