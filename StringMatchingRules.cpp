fb64de647e59b120553bea5a9b9518f833fb3042
Larissa Cutti <cuttilarissa@gmail.com>
2025-04-23 11:27:13 -0300

    "Arrumando o chap3 e algumas anotações"

--- StringMatchingRules.cpp
+++ StringMatchingRules.cpp
@@ -1,23 +1,24 @@
-//#include <bits/stdc++.h> //edit distance, onlu works on gcc, muito grande(tentar retirar)
-//#include <vector>
-//#include <math.h>
-//#include <stdlib.h> //landscape diff
+#include <bits/stdc++.h> //edit distance, only works on gcc, muito grande(tentar retirar)
+#include <vector>
+#include <math.h>
+#include <stdlib.h> //landscape diff
 #include "StringMatchingRules.hpp"
 
-//número de diferentes caracteres entre duas strings
-size_t HammingDistance(const string& x, const string& y){ 
+//----------------------------------------------------------------------------------------------------------------------------------------------
+//HammingDistance -> número de diferentes caracteres entre duas strings
+size_t HammingDistance(const string& x, const string& y){ //Tratar casos em que a string é digitada pelo usuário
     size_t result{0};
     size_t length = x.size();
     for(size_t i = 0; i < length; ++i){
         result += (x[i] ^ y[i]);
     }
     return result;
-} //funcionando para strings digitadas não pelo usuário
-
-//-------------------------------------------------------------------------
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------
+//Binary  distance
 
  //número de 1's que match nas duas strings
-size_t BinaryDistanceA(const string& x, const string& y){
+size_t BDa(const string& x, const string& y){
     size_t result{0};
     size_t length = x.size();
     for(size_t i = 0; i < length; ++i){
@@ -28,7 +29,7 @@ size_t BinaryDistanceA(const string& x, const string& y){
 }
 
 //número de 1's na string x que não match na string y
-size_t BinaryDistanceB(const string& x, const string& y){
+size_t BDb(const string& x, const string& y){
     size_t result{0};
     size_t length = x.size();
     for(size_t i = 0; i < length; ++i){
@@ -39,7 +40,7 @@ size_t BinaryDistanceB(const string& x, const string& y){
 }
 
 //número de 0's na string x que não match na string y
-size_t BinaryDistanceC(const string& x, const string& y){
+size_t BDc(const string& x, const string& y){
     size_t result{0};
     size_t length = x.size();
     for(size_t i = 0; i < length; ++i){
@@ -50,17 +51,17 @@ size_t BinaryDistanceC(const string& x, const string& y){
 }
 
 //número de 0's que match nas duas strings
-size_t BinaryDistanceD(const string& x, const string& y){ 
+size_t BDd(const string& x, const string& y){ 
     size_t result{0};
     size_t length = x.size();
     for(size_t i = 0; i < length; ++i){
-        if((x[i] == 0) && (y[i] == 1))
+        if((x[i] == 0) && (y[i] == 0))
             result++;
     }
     return result;
 }
 
-double RusselRao(const size_t a, const size_t b, const size_t c, const size_t d){
+double RusselRao(const size_t a, const size_t b, const size_t c, const size_t d){ //talvez só um return direto
     double f;
     f = (a / (a + b + c + d));
     return f;
@@ -95,13 +96,11 @@ double Yule(const size_t a, const size_t b, const size_t c, const size_t d){
     f = (((a * d) - (b * c)) / ((a * d) + (b * c)));
     return f;
 }
-
-//----------------------------------------------------------------------------------------
-
-//minimum number of strings transformations required to change s(x) in s(y)
-//ideia retirada do site geekforgeeks
-//verificar tipos
-size_t editDistance(const string& x, const string& y){ //Possui versão recursiva e mais de uma interativa (versão escolhida aparenta ser a mais eficiente)
+//----------------------------------------------------------------------------------------------------------------------------------------------
+//Edit/Levenshtein distance - número minímo de transformações na string necessárias para transformar s(x) em y(x)
+//ideia retirada do site geekforgeeks, verificar tipos
+//Possui versão recursiva e mais de uma interativa (versão escolhida aparenta ser a mais eficiente)
+size_t editDistance(const string& x, const string& y){
     size_t xl = x.size();
     size_t yl = y.size();
 
@@ -125,25 +124,25 @@ size_t editDistance(const string& x, const string& y){ //Possui versão recursiv
     }
     return curr[yl];
 }
+//------------------------------------------------------------------------------------------------------------------------------------------------
+//Value Difference Metric
 
-//--------------------------------------------------------------------------------------------------------
-
-size_t valueDifferenceMetric(const string& x, const string& y, const string& alfabeto){
-    size_t count{0};
-    size_t length = x.size();
-    for(size_t i = 0; i < length; ++i)
-        count += (vdm(x[i], y[i], alfabeto) * weight(x[i], alfabeto)); //verificar se chamar a função assim funciona ou é melhor colocar em variáveis
-    return count;
+//A probabilidade de uma letra pertencer a um alfabeto é de uma entre o tamanho do alfabeto
+//Probabilidade de VDM definido em outro artigo -> soma do número de intancias em um set de treino que tem o valor x para um atributo i e um output c para todas as classes c's
+double P(const size_t length){
+    double p;
+    p = (1 / length); //por outro artigo
+    //aqui seria (número de instâncias em um set de treino que tem o valor x para um atributo a e um output c definido)/(soma do número de instâncias de um set de treino que tem o valor x para um atributo i e um output c para todos os c's em C)
+    return p;
 }
 
-size_t vdm(const char xi, const char yi, const string& C){
+//no artigo o vdm seria apenas essa parte
+size_t vdm(const char xi, const char yi, const size_t length){
     size_t count{0};
-    size_t length = C.size();
     for(size_t c = 0; c < length; ++c)
-        count += pow((P(length) - P(length)), 2); //sempre resulta 0, arrumar
+        count += pow((P(length) - P(length)), 2); //sempre resulta 0, arrumar, pelo artigo aqui nem sempre resultaria 0
     return count;
 }
-
 
 size_t weight(const char xi, const string& C, const size_t length){
     size_t somatorio{0};
@@ -152,28 +151,25 @@ size_t weight(const char xi, const string& C, const size_t length){
 
     return = sqrt(somatorio);
 }
-
-//probabilidade está certa?
-double P(const size_t length){
-    double p;
-    p = (1 / length); //(?)
-    return p;
-}
-
-//--------------------------------------------------------------
-
-size_t diffMatching(const string& x, const string& y){
-    size_t length = x.size();
+size_t valueDifferenceMetric(const string& x, const string& y, const string& alfabeto){
     size_t count{0};
+    size_t length = x.size();
     for(size_t i = 0; i < length; ++i)
+        count += (vdm(x[i], y[i], alfabeto) * weight(x[i], alfabeto)); //verificar se chamar a função assim funciona ou é melhor colocar em variáveis
+    return count;
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//Landscape-Affinity Matching - input e anticorpo strings são dadas como amostras de bytes e então convertidas em valores inteiros positivos para gerar um landscape e então duas strings são comparadas usando uma sliding window
+size_t diffMatching(const string& x, const string& y, const size_t N){
+    size_t count{0};
+    for(size_t i = 0; i < N; ++i)
         count += abs(x[i] - y[i]);
     return count;
 }   
 
-size_t slopeMatching(const string& x, const string& y){
-    size_t length = x.size();
+size_t slopeMatching(const string& x, const string& y, const size_t N){
     size_t count{0};
-    for(size_t i = 0; i < length; ++i){
+    for(size_t i = 0; i < N; ++i){
         size_t s1 = (x[i+1] - x[i]);
         size_t s2 = (y[i+1] - y[i]);
         count += abs(s1 - s2);
@@ -181,34 +177,93 @@ size_t slopeMatching(const string& x, const string& y){
     return count;
 }
 
-size_t physicalMatching(const string& x, const string& y){ //o que é o mi?
-    size_t soma{0}, dif{0}, mini{0}, retorno{0};
-    size_t length = x.size();
-    for(size_t i = 0; i < length; ++i){
-        soma += (x[i+1] - y[i]);
-        mini = std::min((x[i] - y[i]), dif);
+size_t physicalMatching(const string& x, const string& y, const size_t N, const int mi){ //o que é o mi?
+    size_t soma{0}, dif, sub{0}, mini{0};
+    dif = x[0] - y[0];
+    for(size_t i = 0; i < N; ++i){
+        sub = (x[i] - y[i]);
+        mini = std::min(sub, dif);
+        soma += (x[i+1] - y[i]) + 3*mi*mini;
+        if(dif > sub)
+            dif = sub;
     }
-    retorno += soma + (3 * abs(mi) * mini);
-    return retorno;
+    return soma;
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//R-contigous bits matching
+//os bits são iguais nas mesmas posições
+short rcb(const string& x, const string& y, const size_t length, const size_t r){
+    size_t t{0};
+    size_t maiorM{0};
+    for(int i = 0; i < length; ++i){
+        if (x[i] == y[i]){
+            t ++;
+            if (t > maiorM)
+                maiorM = t; //talvez usar essa parametro para comparação de maior match entre duas strings
+            if (t == r)
+                return 1; //verdadeiro, as strings match em um size r
+        }else 
+            t = 0;
+    }
+    return 0;
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//R-Chunk Matching Rule - generalização do rcb
+//não precisa ser na mesma posição
+short rcmr(const string& x, const string& y, const size_t length, const size_t r){
+    size_t t{0};
+    size_t maiorM{0};
+    for(int i = 0; i < length; ++i){
+        for (int j = 1; j < r; j++){
+            if (x[i] == y[i]){
+
+            }
+        }
+    }
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//Real Valued Vector Matching Rule
+size_t euclidianDistance(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length){
+    size_t dist{0}, sub{0};
+    for(int i = 0; i < length; ++i){
+        sub = (x[i] - y[i]);
+        dist += pow(sub, 2);
+    }
+    dist = sqrt(dist);
+    return dist;
 }
 
-//-----------------------------------------------------------------
-//versão que compara de 1 a r em posições aleatórias (A) //talvez isso seja chunk
-void rcbMatchingA(const string& x, const string& y, size_t r){
-
+size_t paticialED(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length, const size_t w){
+    size_t dist{0}, sub{0};
+    for(int i = 0; i < w; ++i){
+        if(i >= length){
+            //exception acabou string
+            return -1;
+        }
+        sub = (x[i] - y[i]);
+        dist += pow(sub, 2);
+    }
+    return dist;
 }
 
-//versão que compara de 1 a r em posições iguais (B)
-void rcbMatchingB(const string& x, const string& y, size_t r){
-
+size_t minkowskiDistance(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length, const size_t l){
+    size_t dist{0}, sub{0};
+    for(int i = 0; i < length; ++i){
+        sub = abs(x[i] - y[i]);
+        dist += pow(sub, l);
+    }
+    dist = pow(dist, (1/l));
+    return dist;
 }
 
-//versão que comprara r em posições aleatórias (C) //talvez isso seja chunk
-void rcbMatchingC(const string& x, const string& y, size_t r){
-
+size_t chebyshevDistance(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length){
+    size_t dist{0}, sub{0};
+    for (int i = 0; i < length; ++i){
+        sub = x[i] - y[i];
+        if (sub > dist)
+            dist = sub;
+    }
+    return dist;
 }
-
-//versão que compara r em posições iguais (D)
-void rcbMatchingD(const string& x, const string& y, size_t r){
-
-}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//Mixed representation
--- StringMatchingRules.hpp
+++ StringMatchingRules.hpp
@@ -4,11 +4,11 @@
 //fractal like shape
 size_t HammingDistance(const string& x, const string& y);
 //---------------------------------------------------------------------------------------
-size_t BinaryDistanceA(const string& x, const string& y);
-size_t BinaryDistanceB(const string& x, const string& y);
-size_t BinaryDistanceC(const string& x, const string& y);
-size_t BinaryDistanceD(const string& x, const string& y);
-//---------------------------------------------------------------------------------------
+size_t BDa(const string& x, const string& y);
+size_t BDb(const string& x, const string& y);
+size_t BDc(const string& x, const string& y);
+size_t BDd(const string& x, const string& y);
+
 double RusselRao(const size_t a, const size_t b, const size_t c, const size_t d);
 double JacardNeedham(const size_t a, const size_t b, const size_t c);
 double Kulzinski(const size_t a, const size_t b, const size_t c);
@@ -18,28 +18,24 @@ double Yule(const size_t a, const size_t b, const size_t c, const size_t d);
 //---------------------------------------------------------------------------------------
 size_t editDistance(const string& x, const string& y);
 //---------------------------------------------------------------------------------------
+//Arrumar lógica
 size_t valueDifferenceMetric(const string x, const string y, const size_t length);
 size_t vdm(const char xi, const char yi, const string C, const size_t length);
 size_t weight(const char xi, const string C, const size_t length);
 double P(const char ai, const string C, const size_t length);
 //---------------------------------------------------------------------------------------
-void diffMatching(const string& x, const string& y);
-void slopeMatching(const string& x, const string& y);
-void physicalMatching();
+void diffMatching(const string& x, const string& y, const size_t N);
+void slopeMatching(const string& x, const string& y, const size_t N);
+void physicalMatching(const string& x, const string& y, const size_t N, const int mi);
 //---------------------------------------------------------------------------------------
-//gridlike shape
-void rcbMatchingA(const string& x, const string& y, size_t r); 
-void rcbMatchingB(const string& x, const string& y, size_t r); 
-void rcbMatchingC(const string& x, const string& y, size_t r); 
-void rcbMatchingD(const string& x, const string& y, size_t r); 
+short rcb(const string& x, const string& y, const size_t length, const size_t r);
 //---------------------------------------------------------------------------------------
-//similar a gridlike shape but simpler
-void rChunkMatching();
+short rcmr(const string& x, const string& y, const size_t length, const size_t r);
 //---------------------------------------------------------------------------------------
 //real-value
-void euclidianDistance();
-void paticialED(); //variation of the euclidian distance (specific window)
-void minkowskiDistance(); //lambda 1 = manhattan d / lambda 2 = euclidian d
+size_t euclidianDistance(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length);
+size_t paticialED(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length, const size_t w); //variation of the euclidian distance (specific window)
+size_t minkowskiDistance(const std::vector<size_t> x, const std::vector<size_t> y, const size_t length, const size_t l); //lambda 1 = manhattan d / lambda 2 = euclidian d
 void chebyshevDistance(); //infinity norm / maximum of the differences for all features
 //---------------------------------------------------------------------------------------
 //mixed
